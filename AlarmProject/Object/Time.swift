a96791991df18aa33412580619c6f52011ef9a29
QuaRang1225 <dbduddnd1225@gmail.com>
2023-12-20 10:40:07 +0900

    #1 - 파일에서 오브젝트 분리
    - 파일에 들어있던 오브젝트들 파일 분리하여 관리(Object폴더)
    - 중복되있던 TimerMode 삭제

---
+++ AlarmProject/Object/Time.swift
@@ -0,0 +1,8 @@
+//
+//  Time.swift
+//  AlarmProject
+//
+//  Created by 유영웅 on 12/20/23.
+//
+
+import Foundation
---
+++ AlarmProject/Object/TimerMode.swift
@@ -0,0 +1,14 @@
+//
+//  Timer.swift
+//  AlarmProject
+//
+//  Created by 유영웅 on 12/20/23.
+//
+
+import Foundation
+
+enum Timer{
+    case run
+    case stop
+    case pause
+}
